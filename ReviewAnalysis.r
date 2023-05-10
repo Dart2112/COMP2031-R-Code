a6f87aac9317ddd952e6ac182e5971e1d0332f7a
Dart2112 <dart2112@lapismc.net>
2023-05-10 12:47:05 +0930

    Added extra plots
    Created new plots
    two for above and below mean
    another for the error distribution

--- ReviewAnalysis.r
+++ ReviewAnalysis.r
@@ -142,7 +142,11 @@ df <- data.frame(givenScores, calculatedScores)
 print(df)
 
 #Write the data to a CSV for safe keeping
-write.csv(df, "/home/benjamin/Documents/Sync/Uni/Data Eng/COMP2031-R-Code/data.csv", row.names=FALSE)
+write.csv(df, "./data.csv", row.names = FALSE)
+
+#
+# Data Visualization
+#
 
 #Make a plot of all items in the data set
 plot(
@@ -156,5 +160,94 @@ plot(
 )
 
 #Split into two plots, One above the mean of calculated scores, one below
+#Calculate the mean of the calculated scores
+meanCalculatedScore = mean(calculatedScores)
+gtmGivenScores = vector()
+ltmGivenScores = vector()
+gtmCalculatedScores = vector()
+ltmCalculatedScores = vector()
+#For each value we check if its above or below the mean, then store both scores in the approriate vector
+for (x in 1:length(calculatedScores)) {
+  if (calculatedScores[x] > meanCalculatedScore) {
+    gtmCalculatedScores <-
+      append(gtmCalculatedScores, calculatedScores[x])
+    gtmGivenScores <- append(gtmGivenScores, givenScores[x])
+  } else{
+    ltmCalculatedScores <-
+      append(ltmCalculatedScores, calculatedScores[x])
+    ltmGivenScores <- append(ltmGivenScores, givenScores[x])
+  }
+}
+#Generate a data frame and flot for just the values above the mean
+gtmDataFrame <- data.frame(gtmGivenScores, gtmCalculatedScores)
+plot(
+  gtmDataFrame$gtmCalculatedScores,
+  gtmDataFrame$gtmGivenScores,
+  type = "p",
+  col = "black",
+  xlab = "Calculated Scores",
+  ylab = "Gives Scores",
+  main = "Greater Than Mean Comparison"
+)
+
+#Do the same as for above the mean but with the less than mean vectors
+ltmDataFrame <- data.frame(ltmGivenScores, ltmCalculatedScores)
+plot(
+  ltmDataFrame$ltmCalculatedScores,
+  ltmDataFrame$ltmGivenScores,
+  type = "p",
+  col = "black",
+  xlab = "Calculated Scores",
+  ylab = "Gives Scores",
+  main = "Less Than Mean Comparison"
+)
+
 
 #Bar graph of the error between calculated and provided
+errorScores = vector()
+intCalculatedScores = as.integer(calculatedScores)
+#Calculate and store the error
+for (x in 1:length(calculatedScores)) {
+  #We only want positive int so we use abs to convert our calculated difference into an always positive int
+  error = abs(intCalculatedScores[x] - givenScores[x])
+  #Then we store the error so that we can graph them in a sec
+  errorScores <- append(errorScores, error)
+}
+
+#Each category counts the number of scores with an error between the category name and the name + 10
+zero = 0
+ten = 0
+twenty = 0
+thirty = 0
+fourty = 0
+fifty = 0
+sixty = 0
+#Loop over the error scores and increment the appropriate category
+for (x in 1:length(errorScores)) {
+  y = errorScores[x]
+  if (y >= 0 && y < 10)
+    zero = zero + 1
+  if (y >= 10 && y < 20)
+    ten = ten + 1
+  if (y >= 20 && y < 30)
+    twenty = twenty + 1
+  if (y >= 30 && y < 40)
+    thirty = thirty + 1
+  if (y >= 40 && y < 50)
+    fourty = fourty + 1
+  if (y >= 50 && y < 60)
+    fifty = fifty + 1
+  if (y >= 60)
+    sixty = sixty + 1
+}
+
+#Store the counts the range titles into a frame for the graph to use
+rangeFrame <-
+  data.frame(
+    ranges = c("0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69"),
+    count = c(zero, ten, twenty, thirty, fourty, fifty, sixty)
+  )
+
+#Generate a plot using the range frame with a centered title and appropriate axis titles
+ggplot(rangeFrame, aes(ranges, count)) +
+  geom_col() + ggtitle("Distribution of Error") + theme(plot.title = element_text(hjust = 0.5)) + xlab("Total Error") + ylab("Listings")
